b6ef08a603cbebce1fb737ae47d79fe6ddfb822c
pepea <pepea.23@live.com>
2019-02-14 20:11:10 +0700

    [UPDATE]  chcek responsive

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import 'phaser';
 import GameScene from './scenes/GameScene';
+import CheckScene from './scenes/CheckScene';
 
 const config = {
     // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
@@ -21,4 +22,36 @@ const config = {
     ]
 };
 
-const game = new Phaser.Game(config);
+const config2 = {
+    // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
+    type: Phaser.WEBGL,
+    pixelArt: true,
+    roundPixels: true,
+    parent: 'content',
+    width: window.screen.width,
+    height: window.screen.height,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 1400 },
+            debug: false
+        }
+    },
+    scene: [
+        CheckScene
+    ]
+};
+
+if(window.screen.width>420){
+    const game = new Phaser.Game(config2);
+}
+
+let interval = setInterval(() => {
+    if(window.screen.width<=420){
+        clearInterval(interval)
+        config.width = window.screen.width
+        config.height = window.screen.height
+        const game = new Phaser.Game(config);
+    }
+}, 1000);
+
---
+++ src/scenes/CheckScene.js
@@ -0,0 +1,19 @@
+
+class CheckScene extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: 'CheckScene'
+        });
+    }
+
+  
+
+    create() {
+       
+        }
+
+    update() {
+        }
+}
+
+export default CheckScene;
